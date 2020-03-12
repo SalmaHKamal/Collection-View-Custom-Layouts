17e79b29be4d67dc95fd65357db751b2cd593f78
Salma Hassan <salmahassan087@gmail.com>
2020-03-12 11:55:57 +0200

    Initial Commit

---
+++ CustomCollectionViewLayouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCollectionViewLayouts
+//
+//  Created by Salma Hassan on 3/12/20.
+//  Copyright © 2020 salma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
